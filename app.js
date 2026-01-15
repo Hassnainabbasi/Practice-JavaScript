168869632b5e93f8b2a7c1a1c202be08c125ff2d
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2026-01-15 11:22:12 +0500

    Hassnain Pushed Code

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// let reverseString = "hello world"
+// let reverse = reverseString.split('').reverse("").join('')
+// console.log(reverse)
+
+// let str = "uju"
+
+// function palindrom(){
+//   let str1 = str.split('').reverse("").join('')
+//   if(str === str1){
+//     console.log('this is palindrom')
+//   }
+//   else{
+//     console.log('this is not palindrom')
+//   }
+// }
+
+// palindrom()
+
+// let max = [2, 5, 10, 9]
+// let maximum = max.sort((index, value) => index - value)
+// console.log(maximum)
+
+
+// let max = [2, 5, 10, 9]
+// let str = Math.max(...max)
+// console.log(str)
