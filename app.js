01e80c74dd0ba46c7dfa5d1b98b3e5dda57d4f7a
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2026-01-15 11:27:36 +0500

    All Question With Concept

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
+// reverse a string
 // let reverseString = "hello world"
 // let reverse = reverseString.split('').reverse("").join('')
 // console.log(reverse)
 
+// check palindrom
 // let str = "uju"
 
 // function palindrom(){
@@ -17,17 +18,17 @@
 
 // palindrom()
 
+//  sort number in array
 // let max = [2, 5, 10, 9]
 // let maximum = max.sort((index, value) => index - value)
 // console.log(maximum)
 
-
+// find max number in array
 // let max = [2, 5, 10, 9]
 // let str = Math.max(...max)
-
 // console.log(str)
 
-
+// remove duplicate from array
 // let duplicate = [1,2,2,3,4,4,9,6,4]
 // let removeduplicate = new Set([...duplicate])
 // console.log(removeduplicate)
