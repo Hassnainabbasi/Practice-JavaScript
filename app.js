e815fbc0a77ca8d865b571317f7ce36b491b5208
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2026-02-10 10:59:20 +0500

    Uncomment various utility functions in app.js

--- app.js
+++ app.js
@@ -32,3 +32,29 @@
 // let duplicate = [1,2,2,3,4,4,9,6,4]
 // let removeduplicate = new Set([...duplicate])
 // console.log(removeduplicate)
+
+// let arr = ["a", "b", "a" , "c", "b"]
+// let sort = new Set(arr)
+// console.log(sort)
+
+// let arr = ["a", "b", "a", "c", "b"]
+// let unique = []
+
+// for(let i = 0; i < arr.length; i++){
+//   if(!unique.includes(arr[i])){
+//     unique.push(arr[i])
+//   }
+// }
+// console.log(unique)
+
+
+// let arr = ["a", "b", "a", "c", "b"]
+// let unique = []
+
+// for(let i = 0; i < arr.length; i++){
+//   if(!unique.includes(arr[i])){
+//     unique.push(arr[i])
+//   }
+// }
+// console.log(unique)
+
